796eef1bd647183ba7b59a98ae8e08a5b5fe9c5a
druvolo1 <104272483+druvolo1@users.noreply.github.com>
2025-01-27 09:42:22 -0500

    1

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def broadcast_ph_readings():
     last_emitted_value = None
     while not stop_event.is_set():
         try:
-            #ph_value = get_latest_ph_reading()  # Get the latest value
-            ph_value = round(get_latest_ph_reading(), 2)  # Round to 2 decimal places
+            ph_value = get_latest_ph_reading()  # Get the latest value
+            ph_value = round(ph_value, 2)  # Round to 2 decimal places
             if ph_value is not None and ph_value != last_emitted_value:
                 last_emitted_value = ph_value
                 socketio.emit('ph_update', {'ph': ph_value})  # Emit the value
