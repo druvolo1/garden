a1cba457f5dc0368d321fc553724af13a842129c
druvolo1 <104272483+druvolo1@users.noreply.github.com>
2025-03-05 22:52:13 -0500

    1

--- api/settings.py
+++ api/settings.py
@@ -17,7 +17,7 @@ settings_blueprint = Blueprint('settings', __name__)
 SETTINGS_FILE = os.path.join(os.getcwd(), "data", "settings.json")
 
 # >>> Define your in-code program version here <<<
-PROGRAM_VERSION = "1.0.24"
+PROGRAM_VERSION = "1.0.25"
 
 # Ensure the settings file exists with default values
 if not os.path.exists(SETTINGS_FILE):
