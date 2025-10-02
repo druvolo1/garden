37425d128d8d0a4bc889e89c7510d7d8977a0bee
druvolo1 <104272483+druvolo1@users.noreply.github.com>
2025-10-02 19:17:24 -0400

    1

--- api/settings.py
+++ api/settings.py
@@ -20,7 +20,7 @@ settings_blueprint = Blueprint('settings', __name__)
 SETTINGS_FILE = os.path.join(os.getcwd(), "data", "settings.json")
 
 # >>> Define your in-code program version here <<<
-CURRENT_VERSION = "1.0.84"
+CURRENT_VERSION = "v1.0.84"
 
 feeding_in_progress = False
 feeding_timer = None
